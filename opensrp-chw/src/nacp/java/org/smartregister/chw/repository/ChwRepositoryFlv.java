ba4cb7e85691511ddc299fcaa54389921f81d5cd
Milton <miltonisaiah@gmail.com>
2024-05-15 14:08:50 +0300

    mvc children registration details linked

--- opensrp-chw/src/nacp/java/org/smartregister/chw/repository/ChwRepositoryFlv.java
+++ opensrp-chw/src/nacp/java/org/smartregister/chw/repository/ChwRepositoryFlv.java
@@ -121,6 +121,9 @@ public class ChwRepositoryFlv {
                 case 29:
                     upgradeToVersion29(db);
                     break;
+                case 30:
+                    upgradeToVersion30(db);
+                    break;
                 default:
                     break;
             }
@@ -497,6 +500,19 @@ public class ChwRepositoryFlv {
             }
         } catch (Exception e) {
             Timber.e(e, "upgradeToVersion28");
+        }
+    }
+
+    private static void upgradeToVersion30(SQLiteDatabase db) {
+        try {
+            // setup reporting
+            ReportingLibrary reportingLibrary = ReportingLibrary.getInstance();
+            String mvcReportIndicatorConfigFile = "config/mvc-monthly-report.yml";
+            for (String configFile : Collections.singletonList(mvcReportIndicatorConfigFile)) {
+                reportingLibrary.readConfigFile(configFile, db);
+            }
+        } catch (Exception e) {
+            Timber.e(e, "upgradeToVersion30");
         }
     }
 }
