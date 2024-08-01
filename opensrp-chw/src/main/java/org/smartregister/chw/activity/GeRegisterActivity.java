9463666aa53c606fac6803b25473d3b51df8e9d0
cozej4 <ilakozejumanne@gmail.com>
2024-08-01 11:22:13 +0300

    :hammer: code refactoring

--- opensrp-chw/src/main/java/org/smartregister/chw/activity/GeRegisterActivity.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/activity/GeRegisterActivity.java
@@ -1,5 +1,6 @@
 package org.smartregister.chw.activity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -7,15 +8,19 @@ import androidx.fragment.app.Fragment;
 
 import com.google.gson.Gson;
 import com.vijay.jsonwizard.activities.JsonFormActivity;
+import com.vijay.jsonwizard.domain.Form;
 import com.vijay.jsonwizard.utils.FormUtils;
 
 import org.json.JSONObject;
 import org.smartregister.AllConstants;
 import org.smartregister.Context;
+import org.smartregister.chw.R;
 import org.smartregister.chw.anc.util.NCUtils;
 import org.smartregister.chw.core.custom_views.NavigationMenu;
 import org.smartregister.chw.fragment.GeRegisterFragment;
+import org.smartregister.chw.fragment.OvcRegisterFragment;
 import org.smartregister.chw.ge.activity.BaseGeRegisterActivity;
+import org.smartregister.chw.ovc.util.Constants;
 import org.smartregister.chw.presenter.GeRegisterActivityPresenter;
 import org.smartregister.clientandeventmodel.Event;
 import org.smartregister.helper.BottomNavigationHelper;
@@ -29,9 +34,27 @@ import java.util.List;
 import timber.log.Timber;
 
 public class GeRegisterActivity extends BaseGeRegisterActivity {
+
+    public static void startRegistration(Activity activity, String baseEntityId, String formName) {
+        Intent intent = new Intent(activity, MvcRegisterActivity.class);
+        intent.putExtra(Constants.ACTIVITY_PAYLOAD.BASE_ENTITY_ID, baseEntityId);
+        intent.putExtra(Constants.ACTIVITY_PAYLOAD.ACTION, Constants.ACTIVITY_PAYLOAD_TYPE.REGISTRATION);
+        intent.putExtra(Constants.ACTIVITY_PAYLOAD.OVC_FORM_NAME, formName);
+
+        activity.startActivity(intent);
+    }
+
     @Override
-    protected void initializePresenter() {
-        presenter = new GeRegisterActivityPresenter();
+    public Form getFormConfig() {
+        Form form = new Form();
+        form.setActionBarBackground(org.smartregister.chw.core.R.color.family_actionbar);
+        form.setWizard(true);
+        form.setName(getString(R.string.ge_enrollment));
+        form.setNavigationBackground(org.smartregister.chw.core.R.color.family_navigation);
+        form.setNextLabel(this.getResources().getString(org.smartregister.chw.core.R.string.next));
+        form.setPreviousLabel(this.getResources().getString(org.smartregister.chw.core.R.string.back));
+        form.setSaveLabel(this.getResources().getString(org.smartregister.chw.core.R.string.save));
+        return form;
     }
 
     @Override
@@ -40,84 +63,7 @@ public class GeRegisterActivity extends BaseGeRegisterActivity {
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        //Obtain an instance of the Navigation menu within our activity
-        NavigationMenu.getInstance(this, null, null);
-
-
-        String baseEntityId = getIntent().getStringExtra("BASE_ENTITY_ID_KEY");
-
-        if (baseEntityId != null) {
-            startFormActivity("ge_enrollment", baseEntityId, null);
-        }
-
-
-    }
-
-    @Override
     protected Fragment[] getOtherFragments() {
-        return new Fragment[0];
-    }
-
-    @Override
-    public void startFormActivity(String formName, String entityId, String metaData) {
-        try {
-            JSONObject jsonObject = (new FormUtils()).getFormJsonFromRepositoryOrAssets(GeRegisterActivity.this, formName);
-            String locationId = Context.getInstance().allSharedPreferences().getPreference(AllConstants.CURRENT_LOCATION_ID);
-            org.smartregister.chw.anc.util.JsonFormUtils.getRegistrationForm(jsonObject, entityId, locationId);
-            startFormActivity(jsonObject);
-        } catch (Exception e) {
-            Timber.e(e);
-        }
-
-    }
-
-    @Override
-    public void startFormActivity(JSONObject jsonObject) {
-        Intent intent = new Intent(this, JsonFormActivity.class);
-        intent.putExtra("json", jsonObject.toString());
-        startActivityForResult(intent, 700);
-    }
-
-    @Override
-    protected void onActivityResultExtended(int requestCode, int resultsCode, Intent intent) {
-        if (requestCode == 700 && resultsCode == RESULT_OK) {
-            String filledForm = intent.getStringExtra("json");
-
-            AllSharedPreferences allSharedPreferences = org.smartregister.chw.util.Utils.getAllSharedPreferences();
-
-            //1. Convert the received filled form into an EVENT
-            Event event = org.smartregister.chw.anc.util.JsonFormUtils.processJsonForm(allSharedPreferences, filledForm, "ec_gender_equality");
-
-            Gson gson = org.smartregister.chw.anc.util.JsonFormUtils.gson;
-            String jsonString = gson.toJson(event);
-
-            try {
-                JSONObject form = new JSONObject(jsonString);
-
-                //2. Save and Process the created EVENT
-                NCUtils.processEvent(event.getBaseEntityId(), form);
-            }catch (Exception e){
-                Timber.e(e);
-            }
-
-        }
-    }
-
-    @Override
-    public List<String> getViewIdentifiers() {
-        return Collections.emptyList();
-    }
-
-    @Override
-    public void startRegistration() {
-
-    }
-
-    @Override
-    protected void registerBottomNavigation() {
-        bottomNavigationHelper = new BottomNavigationHelper();
+        return new Fragment[]{};
     }
 }
